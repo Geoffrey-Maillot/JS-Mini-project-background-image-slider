0bfe485287d030f3b05153c9ba94b3a57a3c45b9
Geoffrey-Maillot <geoffrey.maillot@gmail.com>
2021-01-15 16:47:31 +0100

    challenge done

--- js/app.js
+++ js/app.js
@@ -1,11 +1,33 @@
-//Objectif final Je veux deux boutons qui changent le background-image de la div qui a la classe img-container.
+//On stocke le nom des images dans un tableau
+const img = [
+  `contBcg-0.jpeg`,
+  `contBcg-1.jpeg`,
+  `contBcg-2.jpeg`,
+  `contBcg-3.jpeg`,
+  `contBcg-4.jpeg`,
+]
+// On itinialise un compteur 
+let imgNum = 0
 
-//Stocker les nom des images dans un tableau
+//Je récupère l'élément à modiifier
+const imgContainerElement = document.getElementById(`img-container`)
 
-// Récpérer les boutons et poser un écouteur de type click
+//je récupère et pose un écouteur sur le bouton droit, au clic incrémente le compteur et change d'image en fonction. 
+//Si le compteur est supérieur à la taille du tableau -1 alors on retourne au début du tableau
+const rightBtn = document.getElementById(`btn-right`).addEventListener(`click`, (event) => {
+  
+  imgNum++
+  if (imgNum > img.length - 1) {
+    imgNum = 0
+  }
 
-// Au click, sur le bouton droit change la propriéte "background: url("../img/contBcg-0.jpeg")" par l'élément du tableau
-//qui est à l'index supérieur, si c'est le dernier élément du tableau, met l'élement à l'index 0.
-
-// Au click, sur le bouton droit change la propriéte "background: url("../img/contBcg-0.jpeg")" par l'élément du tableau
-//qui est à l'index inférieur, si c'est le premier élément du tableau, met l'élement du dernier index.
+  imgContainerElement.style.backgroundImage = `url(./img/${img[imgNum]})`;
+})
+//Même logique que bouton droit dans l'autre sens
+const leftBtn = document.getElementById(`btn-left`).addEventListener(`click`, (event) => {
+  if (imgNum === 0) {
+    imgNum = img.length 
+  }
+   imgNum --
+  imgContainerElement.style.backgroundImage = `url(./img/${img[imgNum]})`;
+})
